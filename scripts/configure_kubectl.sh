0d2691bbd4cf633afa4b14437ff9d19fca971de8
Nicolas Brechet <nicolasbrechet@users.noreply.github.com>
2021-09-02 11:02:08 +0200

    Improve kubectl configuration
    * Downloads the proper binary for Linux or Mac OS
    * Copies the k3s configuration to avoid password authentication issue

--- scripts/configure_kubectl.sh
+++ scripts/configure_kubectl.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
 
-# Get kubectl
-curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl -o /tmp/kubectlvagrant
+
+# Get kubectl depending on OS type
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl -o /tmp/kubectlvagrant;;
+    Darwin*)    curl -L https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl -o /tmp/kubectlvagrant;;
+esac
+
 chmod +x /tmp/kubectlvagrant
 
-# Get password from master config file
-PASSWORD=$(vagrant ssh kubemaster1 -c "sudo grep password /etc/rancher/k3s/k3s.yaml" | awk -F':' '{print $2}' | sed 's/ //g')
-
-#Create kubectl config
-cat << EOF > /tmp/kubectlvagrantconfig.yml
-apiVersion: v1
-clusters:
-- cluster:
-    server: https://10.0.0.30:6443
-    insecure-skip-tls-verify: true
-  name: default
-contexts:
-- context:
-    cluster: default
-    user: default
-  name: default
-current-context: default
-kind: Config
-preferences: {}
-users:
-- name: default
-  user:
-    password: ${PASSWORD}
-    username: admin
-EOF
+vagrant ssh kubemaster1 -c "sudo cat /etc/rancher/k3s/k3s.yaml" > /tmp/kubectlvagrantconfig.yml
 
 # Create temp vars to use kubectl with vagrant
 export KUBECONFIG=/tmp/kubectlvagrantconfig.yml
